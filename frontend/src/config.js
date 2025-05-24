f5c9f48c5288b73c0e2a79ca564eb897d6c8bc3c
Dina Kaziyeva <98703053+KDindin@users.noreply.github.com>
2025-05-25 01:51:25 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://192.168.81.145:8000"; //for network
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://fishing-platform-render-app.onrender.com";
